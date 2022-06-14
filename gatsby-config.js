ceea9b7641917fec5f36acde5f85b475484340f8
Rod Macleod <6008271+rodallanmac@users.noreply.github.com>
2022-06-14 11:12:26 +0200

    new update

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    "gatsby-plugin-netlify",
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
